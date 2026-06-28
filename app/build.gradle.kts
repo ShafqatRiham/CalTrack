942ddbaa784bd73a91d89b7e4ea9be9176745bde
ShafqatRiham <tuq93822@temple.edu>
2026-06-28 23:16:00 +0900

    Change build.gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.caltrack"
-        minSdk = 37
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
