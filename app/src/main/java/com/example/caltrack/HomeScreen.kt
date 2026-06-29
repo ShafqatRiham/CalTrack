d30e09e999486249959f2732bd795760b817431f
harrisoncheung <calecheung@gmail.com>
2026-06-29 15:04:46 +0900

    Update HomeScreen.kt
    List wip, Home Bar wip

--- app/src/main/java/com/example/caltrack/HomeScreen.kt
+++ app/src/main/java/com/example/caltrack/HomeScreen.kt
@@ -1,20 +1,40 @@
 package com.example.caltrack
 
+import android.widget.ListView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.caltrack.ui.theme.Purple40
+
+var foodList = mutableListOf("Egg", "Bacon", "Bread")
 
 @Composable
 fun HomeScreen() {
+
+    LazyColumn(modifier = Modifier, verticalArrangement = Arrangement.Center) {
+        foodList
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Home")
+        Text(text = "Gnome")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth().background(color = Purple40),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Buttons and stuff")
     }
 }
